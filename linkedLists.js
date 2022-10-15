c29781eb6cdef8d732c03d1bf7183af1afeffb11
hattima-tim <irfanridwan20@gmail.com>
2022-10-15 03:09:20 +0000

    add size function

--- linkedLists.js
+++ linkedLists.js
@@ -1,40 +1,52 @@
 function Node(value, nextNode) {
-    return {value,next:nextNode}
+  return { value, next: nextNode };
 }
 
 function LinkedList() {
-    let list = {
-        value: 1,
+  let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
         next: {
-            value: 2,
-            next: {
-                value: 3,
-                next: {
-                    value: 4,
-                    next: null
-                }
-            }
-        }
-    };
+          value: 4,
+          next: null,
+        },
+      },
+    },
+  };
 
-    function append(value){
-        if(!value && value!==0) return;
+  function append(value) {
+    if (!value && value !== 0) return;
 
-        let tmp = list;
-        while (tmp.next !== null) {
-            tmp = tmp.next;
-        }
-        tmp.next = Node(value, null)
-        console.log(list)
+    let tmp = list;
+    while (tmp.next !== null) {
+      tmp = tmp.next;
     }
+    tmp.next = Node(value, null);
+    console.log(list);
+  }
 
-    function prepend(value) {
-        if (!value && value !== 0) return;
-        list = Node(value, list)
-        console.log(list)
+  function prepend(value) {
+    if (!value && value !== 0) return;
+    list = Node(value, list);
+    console.log(list);
+  }
+
+  function size() {
+    let nodeCount = 0;
+    let tmp = list;
+    while (tmp !== null) {
+      nodeCount += 1;
+      tmp = tmp.next;
     }
-    return {append,prepend}
+    return nodeCount;
+  }
+  return { append, prepend, size };
 }
 
-LinkedList().append(10)
-LinkedList().prepend(0)
+const linkedList = LinkedList();
+linkedList.append(10);
+linkedList.prepend(0);
+console.log(linkedList.size()); //6
